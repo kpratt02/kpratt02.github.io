3302fa6efbf80b462ac669c060c29a0e88246e55
kpratt02 <31668425+kpratt02@users.noreply.github.com>
2024-07-16 11:40:21 -0400

    removing extra folders

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HRQXTH+lfYLuIIWbBxdtBvoKbNw+hqdHCZ97OSot29s=\"\n}"
--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -10,8 +10,7 @@ function Footer() {
             <a href='mailto:kiley.pratt@yale.edu'>
             <button>
               <img src="https://cdn-icons-png.flaticon.com/128/3178/3178158.png" alt="Email Icon" className="footer-icon" />
-              Email
-              <p>kiley.pratt@yale.edu</p>
+              kiley.pratt@yale.edu
             </button>
             </a>
           </div>
@@ -19,8 +18,7 @@ function Footer() {
             <a href='tel:+18028258972'>
               <button>
                 <img src="https://cdn-icons-png.flaticon.com/128/455/455705.png" alt="Phone Icon" className="footer-icon" />
-                Call me, beep me
-                <p>(802) 825 - 8972</p>
+                (802) 825 - 8972
               </button>
             </a>
           </div>
@@ -28,8 +26,7 @@ function Footer() {
             <a href='https://www.linkedin.com/in/kiley-pratt/' target='_blank'>
               <button>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" alt="LinkedIn Icon" className="footer-icon" />
-                LinkedIn
-                <p>@Kiley-Pratt</p>
+                @Kiley-Pratt
               </button>
             </a>
           </div>
@@ -37,8 +34,7 @@ function Footer() {
             <a href='https://github.com/kpratt02' target='_blank'>
               <button>
                 <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="GitHub Icon" className="footer-icon" />
-                GitHub
-                <p>@kpratt02</p>
+                @kpratt02
               </button>
             </a>
           </div>
